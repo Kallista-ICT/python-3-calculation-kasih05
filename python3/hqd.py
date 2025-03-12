730bab85f3196995a1aed3d6b2b7475d113d9028
kasih05 <kasihmaria15@gmail.com>
2025-03-12 01:13:01 +0000

    Submit

---
+++ python3/hqd.py
@@ -0,0 +1,7 @@
+
+distance = float(input("enter the distance(in meters):  "))
+time = float(input("enter the time(in seconds):  "))
+
+velocity = distance / time
+
+print(f"The velocity is {velocity :.2f} m/s.")
